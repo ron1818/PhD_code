12669b7a8753b2d70429e12ccedf1fe0ac1e2995
ron1818 <renye.china@gmail.com>
2015-09-02 13:47:59 +0800

    add ciel code
    ciel 2014 and 2013

---
+++ CIEL2013/knn_svm_CIEL2013.R
@@ -0,0 +1,117 @@
+# how to cite
+# @INPROCEEDINGS{Ren2013CIEL,
+#                author = {Ye Ren and Le Zhang and P. N. Suganthan},
+#                title = {K{NN} based bagging {SVM} pruning},
+#                booktitle = {Proc. IEEE Symposium on Computational Intelligence and Ensemble Learning
+#                             (CIEL'13)},
+#   year = {2013},
+#   address = {Singapore},
+#   month = apr
+# }
+
+setwd("./CIEL2013_2014")
+# 0) initialize
+require(xts)
+require(e1071)
+require(bootstrap)
+require(RSNNS)
+require(timeDate)
+require(forecast)
+source('../misc/pre_processing_fn.R')
+source('../misc/post_processing_fn.R')
+source('../misc/cross_validation_fn.R')
+source('../method/svm_fn.R')
+source('../method/knn_fn.R')
+
+# load data
+breast.wisconsin<-read.csv('breast_wisconsin.csv')
+labels<-breast.wisconsin[,'class'] #2 bengin, 4 malignant
+labels[which(labels==2)]=-1 #bengin
+labels[which(labels==4)]=1 #maligant
+breast.wisconsin[,'class']<-labels # convert to -1, +1
+
+# find complete cases
+breast.wisconsin<-breast.wisconsin[complete.cases(breast.wisconsin),]
+# scale data
+data<-scale(breast.wisconsin[,2:10]) # avoid id
+labels<-breast.wisconsin[,'class']
+
+# split to training and testing
+RATIO=0.7
+trn.number<-round(nrow(data)*RATIO)
+trn.idx<-sample(nrow(data),trn.number)
+tst.idx<--1*trn.idx
+
+trn.data<-data[trn.idx,]
+trn.labels<-labels[trn.idx]
+tst.data<-data[tst.idx,]
+tst.labels<-labels[tst.idx]
+
+# bootstrap
+B=100
+idx.length<-nrow(trn.data)
+boot<-bootstrap(1:idx.length, B, as.numeric)
+idx.boot<-boot$thetastar # oob.idx=-idx.boot
+
+# training
+model<-list()
+prediction<-probability<-array(NA,c(nrow(trn.data),B))
+for ( i in 1:B){
+	model[[i]]<-svm(trn.data[idx.boot[,i],],trn.labels[idx.boot[,i]],type='C-classification',probability=TRUE)
+	tmp<-predict(model[[i]],trn.data,probability=TRUE)
+	prediction[,i]<-as.numeric(levels(tmp)[tmp])
+	probability[,i]<-apply(attr(tmp,'probabilities'),1,max)
+}
+
+# identify wrong classified trn.data~bag pair
+wrong.classify<-which(prediction!=matrix(rep(trn.labels,B),ncol=B,byrow=FALSE),arr.ind=TRUE)
+colnames(wrong.classify)<-c('data','bag')
+wrong.classify.mat<-array(TRUE,c(nrow(trn.data),B))
+wrong.classify.mat[wrong.classify]=FALSE
+
+# self pruning
+self.pruning.mat<-array(NA,c(nrow(trn.data),B))
+for(i in 1:B)
+	self.pruning.mat[,i]<-is.element(1:nrow(trn.data),idx.boot[,i])
+
+self.pruning<-which(self.pruning.mat==TRUE,arr.ind=TRUE)
+colnames(self.pruning)<-c('data','bag')
+
+# make probability at wrong classify infinity
+probability[wrong.classify]=-Inf
+# make probability at self contained infinity
+probability[self.pruning]=-Inf
+
+
+# sort probability
+r=21
+probability.idx<-probability.dist<-array(NA,c(nrow(trn.data),r))
+for (i in 1:nrow(trn.data)){
+	probability.idx[i,]<-order(probability[i,],decreasing=TRUE)[1:r]
+	probability.dist[i,]<-probability[i,probability.idx[i,]]
+}
+
+
+
+# knn pruning
+k=31
+knn.dist.mat<-my_dist(trn.data,tst.data,k=k) #|tst|*|trn|(k)
+
+# testing
+tst.predict<-array(NA,c(nrow(tst.data),k*r))
+for (i in 1:nrow(tst.data)){
+	ith.trn.idx<-knn.dist.mat$knn.idx[i,]
+	ith.bag.idx<-probability.idx[ith.trn.idx,]
+	counter=1
+	for (b in ith.bag.idx){	
+		tmp<-predict(model[[b]],tst.data)
+		tst.predict[,counter]<-as.numeric(levels(tmp)[tmp])
+		counter=counter+1
+	}
+}
+
+#aggregate
+aggregated.tst.predict<-my_majority_vote(tst.predict[,1:k*r])
+#error measure
+my_classification_measure(aggregated.tst.predict$value,tst.labels)
+
---
+++ CIEL2014/emd_adaboost_nn_CIEL2014.R
@@ -0,0 +1,85 @@
+# how to cite
+# @INPROCEEDINGS{Ren2014CIEL,
+#                author = {Ye Ren and Xueheng Qiu and P. N. Suganthan},
+#                title = {{EMD} based AdaBoost-{BPNN} Method for Wind Speed Forecasting},
+#                booktitle = {Proc. IEEE Symposium on Computational Intelligence and Ensemble Learning
+#                             (CIEL'14)},
+#   year = {2014},
+#   address = {Orlando, US},
+#   month = dec
+# }
+
+# setwd("work/CIEL2013_2014")
+# 0) initialize
+require(RSNNS)
+require(MASS)
+require(xts)
+require(timeDate)
+require(forecast)
+require(EMD)
+source('../AdaBoost/adaboost_fn.R')
+source('../misc/pre_processing_fn.R')
+source('../misc/post_processing_fn.R')
+source('../misc/cross_validation_fn.R')
+source('../method/mlp_fn.R')
+
+# 4) read dataset
+wind<-read.csv('41004h2011.csv')
+wind.xts<-xts(wind[,'WSPD'],timeDate(wind[,1]))
+org.data.xts<-wind.xts['2011-01-01/2011-01-31']
+
+# 5) define parameter
+HORIZON=5
+LAG=24
+RATIO=0.7
+MAX.IMF=5
+n_h_range=seq(LAG,2*LAG,6)
+
+# 10) scale
+data.xts<-my_scale_ts(org.data.xts,c(0,1))$scaled
+
+# 20) split
+tmp<-my_vector_split(data.xts,HORIZON=HORIZON,LAG=LAG,RATIO=RATIO)
+trn<-tmp$trn
+tst<-tmp$tst
+
+# 30) EMD to extract data
+emd.xts<-emd(as.numeric(data.xts),max.imf=MAX.IMF)
+nimf<-emd.xts$nimf
+
+# 40) partition into trn and tst
+emd.trn<-emd.tst<-list()
+imf.ada.predict<-imf.ann.predict<-array(NA,dim=c(nrow(tst$labels),ncol(tst$labels),nimf+1))
+for (i in 1:(nimf+1)){ # for each IMF, +1 is for residue
+	if(i>nimf){ # for residue
+		tmp<-my_vector_split(emd.xts$residue,HORIZON=HORIZON,LAG=LAG,RATIO=RATIO)
+	}else{ # for imf
+		tmp<-my_vector_split(emd.xts$imf[,i],HORIZON=HORIZON,LAG=LAG,RATIO=RATIO)
+	}
+	emd.trn$data[[i]]<-tmp$trn$data
+	emd.trn$labels[[i]]<-tmp$trn$labels
+	emd.tst$data[[i]]<-tmp$tst$data
+	emd.tst$labels[[i]]<-tmp$tst$labels
+	
+	# 50) cv knn to determine best k and d
+	emd.ann.model<-my_cv_ann(tmp$trn, tmp$tst, n_h_range, method='mlp', is.ts=TRUE)
+	imf.ann.predict[,,i]<-emd.ann.model$predict
+	
+	# 60) loop on HORIZON for adaboost training
+	emd.ada.model<-list()
+	for (h in 1:HORIZON){
+		emd.ada.model[[h]]<-AdaBoost.R2(emd.trn$data[[i]],emd.trn$labels[[i]][,h],emd.tst$data[[i]],method='mlp',maxIter=10)
+		imf.ada.predict[,h,i]<-emd.ada.model[[h]]$aggregated.predict
+	}
+	
+}
+# 70) take sum of IMFs
+emd.ada.predict<-emd.ann.predict<-array(NA,dim=c(nrow(tst$labels),HORIZON))
+for (h in 1:HORIZON){
+	emd.ann.predict[,h]<-apply(imf.ann.predict[,h,],1,sum)
+	emd.ada.predict[,h]<-apply(imf.ada.predict[,h,],1,sum)
+}
+
+# 80) error measure
+emd.ann.error<-my_forecasting_measure(emd.ann.predict,tst$labels,tst$data[,LAG])
+emd.ada.error<-my_forecasting_measure(emd.ada.predict,tst$labels,tst$data[,LAG])
